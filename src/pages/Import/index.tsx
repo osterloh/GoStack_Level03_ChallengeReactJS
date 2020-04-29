762e12fa10c6083cb835f12400cd36eb1d4f8819
Johnatan Osterloh <johnatanosterloh@gmail.com>
2020-04-29 18:49:12 -0300

    Desafio Fundamentos ReactJS, projeto web desenvolvido utilizando a tecnologia ReactJS co TypeScript, utilizando rotas para consumir a API desenvolvida em Node.js, listando os dados cadastrados no banco de dados e importando arquivos CSV para gerar novos registros

--- src/pages/Import/index.tsx
+++ src/pages/Import/index.tsx
@@ -23,19 +23,29 @@ const Import: React.FC = () => {
   const history = useHistory();
 
   async function handleUpload(): Promise<void> {
-    // const data = new FormData();
-
-    // TODO
+    const data = new FormData();
 
     try {
-      // await api.post('/transactions/import', data);
+      uploadedFiles.forEach(async file => {
+        data.append('file', file.file);
+      });
+
+      await api.post('/transactions/import', data);
     } catch (err) {
-      // console.log(err.response.error);
+      console.log(err.response.error);
     }
   }
 
   function submitFile(files: File[]): void {
-    // TODO
+    files.map((file): void => {
+      const newFile: FileProps = {
+        file,
+        name: file.name,
+        readableSize: filesize(file.size),
+      };
+
+      setUploadedFiles([...uploadedFiles, newFile]);
+    });
   }
 
   return (
